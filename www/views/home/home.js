68fee0e7da9a3a2d8f2d3ffee91f5052b78ee520
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-03-09 22:22:18 +0500

    structure is almost done

---
+++ www/views/home/home.js
@@ -0,0 +1,24 @@
+/**
+ * Created by malikasinger on 19/08/2015.
+ */
+angular.module("starter")
+
+  .controller('homeController', function ($scope /*,$http, $ionicLoading, $ionicSideMenuDelegate, $ionicPopup, $timeout*/) {
+
+    var ref = new Firebase("https://inzi-chat-app.firebaseio.com");
+
+    $scope.authWithFacebook = function(){
+
+      ref.authWithOAuthPopup("facebook", function(error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+        } else {
+          console.log("Authenticated successfully with payload:", authData);
+        }
+      });
+
+    }
+
+
+  });
+
