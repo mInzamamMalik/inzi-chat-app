0e5ec3bc4c04f5d5727b8e177e872ca552edd87a
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-03-16 21:46:30 +0500

    message send hogya bs display krwana reh gya hai inbox mai

--- www/views/home/home.js
+++ www/views/home/home.js
@@ -3,13 +3,29 @@
  */
 angular.module("starter")
 
-  .controller('homeController', function ($scope,universalService /*,$http, $ionicLoading, $ionicSideMenuDelegate, $ionicPopup, $timeout*/) {
+  .controller('homeController', function ($scope, universalService /*,$http, $ionicLoading, $ionicSideMenuDelegate, $ionicPopup, $timeout*/) {
 
 
-    $scope.authWithFacebook = function()
-    {
+    $scope.authWithFacebook = function () {
       universalService.authWithFacebook()
     }
+
+
+    // Now what happens when you want to retrieve
+    // all the comments associated with a particular link
+
+    //var commentsRef = new Firebase("https://malikasinger-practiceapp.firebaseio.com/comments");
+    //var linkRef = new Firebase("https://malikasinger-practiceapp.firebaseio.com/links");
+    //
+    //var LCommentsRef = linkRef.child("link1").child("comments");
+    //
+    //LCommentsRef.on("child_added", function (snap) {
+    //  commentsRef.child(snap.key()).once("value", function (snap) {
+    //    // Render the comment on the link page.
+    //    console.log("this is a comment",snap.val());
+    //  });
+    //});
+
 
   });
 
